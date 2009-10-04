c8ba16a66a064437f1bc616ceac60d30589008cf
George Notaras <gnot@g-loaded.eu>
2009-10-04 10:20:32 +0000

    Initial code import

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Having installed the 'delayed-shutdown' initscript, run:
+#
+#   test.sh &
+#
+# Now reboot or shutdown your machine. It should wait until
+# /var/lock/noshutdown.lock is deleted and then proceed to shutdown.
+#
+lockfile /var/lock/noshutdown.lock
+sleep 90
+rm -f /var/lock/noshutdown.lock
+exit 0
+
